1a445a2aea9f1ba8e554bba114d3224a45239e16
Gaurav Gupta <Gaurav.Gupta@Reval.com>
2018-04-13 21:37:09 +0530

    First MVC Action.

---
+++ Vidly2/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "DDLJ" };
+            return View(movie);
+        }
+    }
+}
